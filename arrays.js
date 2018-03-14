037aec989248f097c0f8be1887e0ef3c0b05cbcf
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-14 23:42:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  return [array, ...element]
+  return [array, ...element];
 }
 
 function destructivelyAddElementToBeginningOfArray() {
-  array.unshift
+  array.unshift(element);
+  return array;
 }
